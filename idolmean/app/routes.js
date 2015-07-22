688f29385c774848d59a3c911132b928bb3d40d4
fantasticy <iamyoungrok@gmail.com>
2015-07-22 20:51:21 +0900

    Complete idolmean

--- idolmean/app/routes.js
+++ idolmean/app/routes.js
@@ -6,7 +6,7 @@ module.exports = function(app) {
   // server routes ===========================================================
   // handle things like api calls
   // authentication routes
-  
+
   // sample api route
   app.get('/api/nerds', function(req, res) {
     // use mongoose to get all nerds in the database
@@ -27,7 +27,7 @@ module.exports = function(app) {
   // frontend routes =========================================================
   // route to handle all angular requests
   app.get('*', function(req, res) {
-    res.sendfile('./public/views/index.html'); // load our public/index.html file
+    res.sendfile('./public/index.html'); // load our public/index.html file
   });
 
 };
